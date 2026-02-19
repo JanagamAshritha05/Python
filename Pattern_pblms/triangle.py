93763d1e0cbfa66c551b0b4f031d2ff6c5bdae52
Ashritha <janagamashrithareddy@gmail.com>
2026-02-19 12:51:08 +0530

    triangle

---
+++ Pattern_pblms/triangle.py
@@ -0,0 +1,39 @@
+
+n=4 
+
+for i in range(1,n+1):
+    print("* "*i)
+
+
+# 
+n=4 
+
+for i in range(1, n+1):
+    new=""
+    for j in range(1,i+1):
+        new+="* "
+    print(new)
+
+#
+n=4 
+
+for i in range(1,n+1):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print() 
+
+# 
+n=4 
+
+i=0 
+
+while i<n:
+    
+    j=0 
+    while j<i+1:
+        print("*", end=" ")
+        j+=1 
+    print() 
+    i+=1 
+
+    
