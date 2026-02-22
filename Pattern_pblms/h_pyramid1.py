4039b3e5f5a1ad9d0825132c9b34d4c45d7e9513
Ashritha <janagamashrithareddy@gmail.com>
2026-02-22 11:33:28 +0530

    h_pyramid1

---
+++ Pattern_pblms/h_pyramid1.py
@@ -0,0 +1,66 @@
+n = 5
+
+for i in range(1, n+1):
+    if i==1:
+        print("* "*n) 
+    elif i==n:
+        print(" "*(n-1) + "*") 
+    else:
+        print(" "*(i-1) + "* " + "  "*(n-i-1) + "*")
+
+
+#
+n = 5
+
+i = 1
+while i<=n:
+    if i==1:
+        print("* "*n) 
+    elif i==n:
+        print(" "*(n-1) + "*") 
+    else:
+        print(" "*(i-1) + "* " + "  "*(n-i-1) + "*") 
+    i+=1 
+
+
+#
+n = 5
+
+for i in range(1, n+1):
+    row=""
+    left_spaces=" "*(i-1) 
+    for j in range(1, n-i+2):
+        if i==1:
+            row+="* " 
+        elif j==1 or j==n-i+1:
+            row+="* " 
+        else:
+            row+="  " 
+    print(left_spaces + row) 
+
+
+#
+n = 5 
+
+i=1 
+while i<=n:
+    row=""
+    left_spaces=" "*(i-1) 
+    j=1 
+    while j<=n-i+1:
+        if i==1:
+            row+="* " 
+        elif j==1 or j==n-i+1:
+            row+="* " 
+        else:
+            row+="  " 
+        j+=1 
+    print(left_spaces + row) 
+    i+=1 
+
+
+
+
+
+
+
