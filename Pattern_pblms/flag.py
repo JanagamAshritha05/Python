e889cb67b75283988783b6f0206ef1ff84d6adae
Ashritha <janagamashrithareddy@gmail.com>
2026-02-27 10:57:38 +0530

    flag

---
+++ Pattern_pblms/flag.py
@@ -0,0 +1,35 @@
+
+n=5
+
+for i in range(1, n*2-1):
+    row="" 
+    for j in range(1, n+1):
+        if i<n:
+            row+="* " 
+        elif j==1:
+            row+="* " 
+        else:
+            row+="  " 
+    print(row) 
+
+
+
+# 
+n=5 
+
+i=1 
+while i<n*2-1:
+    row="" 
+    j=1 
+    while j<=n:
+        if i<n:
+            row+="* " 
+        elif j==1:
+            row+="* " 
+        else:
+            row+="  " 
+        j+=1 
+    print(row)
+    i+=1 
+
+
