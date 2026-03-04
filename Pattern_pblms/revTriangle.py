bcd90cbc4859ca59de0fad836a4d2cf9b428a1ae
Ashritha <janagamashrithareddy@gmail.com>
2026-03-04 12:44:52 +0530

    revTriangle

---
+++ Pattern_pblms/revTriangle.py
@@ -0,0 +1,27 @@
+
+n=5
+order=65 
+for i in range(1, n+1):
+    row="" 
+    for j in range(i):
+        row+=chr(order + n-j-1) + " " 
+    print(row)
+
+# 
+n=5 
+order=65 
+i=1 
+while i<=n:
+    row="" 
+    j=0 
+    while j<i:
+        row+=chr(order + n-j-1) + " " 
+        j+=1 
+    print(row)
+    i+=1 
+    
+    
+    
+    
+    
+
