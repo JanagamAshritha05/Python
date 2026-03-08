ea66073850a4d4296ef4203302ec87229de1394e
Ashritha <janagamashrithareddy@gmail.com>
2026-03-08 19:03:50 +0530

    sqr

---
+++ Pattern_pblms/Number_Patterns/sqr.py
@@ -0,0 +1,20 @@
+n=5
+for i in range(1, n+1):
+    row=""
+    for j in range(1, n+1):
+        row+=str(j)+" "
+    print(row)
+
+
+# 
+n=5 
+i=1 
+while i<=n:
+    row=""
+    j=1 
+    while j<=n:
+        row+=str(j)+" "
+        j+=1 
+    print(row)
+    i+=1 
+    
