92e7a7ad54ab0c3e94f0ba41161d5158bb27ca0d
Ashritha <janagamashrithareddy@gmail.com>
2026-03-09 10:51:30 +0530

    sqr1

---
+++ Pattern_pblms/Number_Patterns/sqr1.py
@@ -0,0 +1,21 @@
+
+n=5
+for i in range(1, n+1):
+    row=""
+    for j in range(1, n+1):
+        row+=str(i)+" "
+    print(row)
+
+# 
+n=5 
+i=1 
+while i<=n:
+    row=""
+    j=1 
+    while j<=n:
+        row+=str(i)+" " 
+        j+=1 
+    print(row)
+    i+=1 
+    
+
