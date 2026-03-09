9faf39549a4d8d0a8454f5763cbe38b54cebdf45
Ashritha <janagamashrithareddy@gmail.com>
2026-03-09 15:11:04 +0530

    diamond

---
+++ Pattern_pblms/Number_Patterns/diamond.py
@@ -0,0 +1,65 @@
+
+n=5
+for i in range(1, n+1):
+    row=""
+    for j in range(1, i+1):
+        row+=str(j)+" "
+    
+    right=""
+    for j in range(1, i):
+        right=str(j)+" "+right
+    print("  "*(n-i)+row+right)
+    
+for i in range(1, n):
+    row=""
+    for j in range(1, n-i+1):
+        row+=str(j)+" " 
+    
+    right=""
+    for j in range(1, n-i):
+        right=str(j)+" "+right 
+    print("  "*i+row+right)
+    
+    
+# 
+n=5 
+i=1 
+while i<=n:
+    row=""
+    j=1 
+    while j<=i:
+        row+=str(j)+" "
+        j+=1 
+        
+    right=""
+    j=1 
+    while j<i:
+        right=str(j)+" "+right
+        j+=1 
+    print("  "*(n-i)+row+right)
+    i+=1 
+       
+i=1
+while i<n:
+    row=""
+    j=1 
+    while j<=n-i:
+        row+=str(j)+" "
+        j+=1 
+    
+    right=""
+    j=1 
+    while j<n-i:
+        right=str(j)+" "+right 
+        j+=1 
+    print("  "*i + row + right)
+    i+=1 
+    
+    
+    
+    
+    
+    
+    
+    
+    
