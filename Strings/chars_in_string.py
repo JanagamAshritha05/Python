6e9dcf3baea6561f9af69827f9377efb9c34bacb
Ashritha <janagamashrithareddy@gmail.com>
2026-03-16 18:57:49 +0530

    chars_in_string

---
+++ Strings/chars_in_string.py
@@ -0,0 +1,25 @@
+s="python"
+count=0 
+
+for char in s:
+    count+=1 
+print(count) 
+
+#
+s="python"
+count=0 
+i=0 
+while i<len(s):
+    count+=1 
+    i+=1 
+print(count)
+
+#
+s="python"
+print(len(s))
+
+#
+s="python" 
+print(sum(1 for char in s))
+
+
