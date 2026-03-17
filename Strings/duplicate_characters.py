49bfb65f3118a0360cd18384caa5022ba4b4f997
Ashritha <janagamashrithareddy@gmail.com>
2026-03-17 09:54:45 +0530

    duplicate_characters

---
+++ Strings/duplicate_characters.py
@@ -0,0 +1,50 @@
+
+s="programming"    
+new=""
+
+for char in s:
+    count=s.count(char) 
+    if count>1:
+        new+=char
+
+for char in set(new):
+    print(char)
+
+    
+#
+s="programming"
+freq={}
+count=0
+for char in s:
+    if char in freq:
+        freq[char]+=1 
+    else:
+        freq[char]=1 
+
+for char, count in freq.items():
+    if count>1:
+        print(char)
+    
+#
+s = "programming"
+freq = {}
+i = 0
+while i < len(s):
+    char = s[i]
+    if char in freq:
+        freq[char] += 1
+    else:
+        freq[char] = 1
+    i += 1
+
+for char, count in freq.items():
+    if count > 1:
+        print(char)
+
+
+
+
+
+
+
+
