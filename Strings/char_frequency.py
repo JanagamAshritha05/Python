d35932644478745a6f2d8e99e6445491c7226a96
Ashritha <janagamashrithareddy@gmail.com>
2026-03-17 08:21:39 +0530

    char_frequency

---
+++ Strings/char_frequency.py
@@ -0,0 +1,44 @@
+
+s="banana"
+
+for char in set(s):
+    count=s.count(char) 
+    print(char+": "+str(count)) 
+    
+# 
+
+s="banana"
+freq={}
+
+for char in s:
+    if char in freq:
+        freq[char]+=1 
+    else:
+        freq[char]=1 
+        
+for char, count in freq.items():
+    print(char + ": " + str(count))
+    
+    
+#
+s="banana"
+freq={}
+i=0
+while i<len(s):
+    char=s[i] 
+    if char in freq:
+        freq[char]+=1 
+    else:
+        freq[char]=1 
+    i+=1 
+    
+for char, count in freq.items():
+    print(char + ": " + str(count))
+
+    
+    
+    
+    
+    
+    
+    
