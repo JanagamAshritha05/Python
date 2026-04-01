35926f1b5f4e895d986e4f659ca8d26b8f6a6487
Ashritha <janagamashrithareddy@gmail.com>
2026-04-01 09:29:44 +0530

    subString

---
+++ Strings/subString.py
@@ -0,0 +1,39 @@
+
+s="abc"
+
+for i in range(len(s)):
+    for j in range(i+1, len(s)+1):
+        print(s[i:j])
+
+
+#
+s="abc"
+res=[]
+
+for i in range(len(s)):
+    for j in range(i+1, len(s)+1):
+        res.append(s[i:j]) 
+print(res)
+    
+
+#
+s="abc"
+i=0 
+while i<len(s):
+    j=i+1 
+    while j<=len(s):
+        print(s[i:j])
+        j+=1 
+    i+=1 
+
+
+'''
+Find all substrings of a string
+i/p:abc  o/p:   a
+                ab
+                abc
+                b
+                bc
+                c
+
+'''
