fdec7aefda1b21f4195d1c4651c8f3720ee52051
Ashritha <janagamashrithareddy@gmail.com>
2026-06-10 12:21:19 +0530

    sortdictwithval

--- Dictionaries/Basic/Basic level/sortdictwithval.py
+++ Dictionaries/Basic/Basic level/sortdictwithval.py
@@ -1,4 +1,20 @@
 d = {"a": 3, "b": 1, "c": 2, "d": 5}  
 
+items = list(d.items()) 
+
+for i in range(len(items)):
+    for j in range(i+1, len(items)):
+        if items[i][1] > items[j][1]:
+            items[i], items[j] = items[j], items[i] 
+             
+res = {}
+
+for k, v in items:
+    res[k] = v 
+
+print(res) 
+
+#  
+
 
 
