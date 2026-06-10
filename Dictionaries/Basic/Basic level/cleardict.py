52cec6018eca4e18373573b8109b9eb3d4eb6087
Ashritha <janagamashrithareddy@gmail.com>
2026-06-10 10:06:58 +0530

    cleardict

---
+++ Dictionaries/Basic/Basic level/cleardict.py
@@ -0,0 +1,18 @@
+d = {"name": "Ashritha", "age": 23, "city": "Vijayawada"}
+
+d.clear()
+
+print(d) 
+
+#
+
+d = {"name": "Ashritha", "age": 23, "city": "Vijayawada"}
+
+del d
+
+d = {}
+
+print(d) 
+
+
+
