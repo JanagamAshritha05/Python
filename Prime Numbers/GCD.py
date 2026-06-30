db32f1a8b5c7e15498695dc2eafa57a0a942092f
Ashritha <janagamashrithareddy@gmail.com>
2026-06-30 09:43:32 +0530

    updated gcd

--- Prime Numbers/GCD.py
+++ Prime Numbers/GCD.py
@@ -8,3 +8,17 @@ print(gcd)
 #4      //  2   #16     //  1
 #6              #9
 
+
+'''
+gcd - greatest common divisor of two or more no's or we can say greatest common factor
+or highest common factor(HCF)
+
+ex: gcd(12, 18)
+
+factors of 12 = 1, 2, 3, 4, 6, 12
+factors of 18 = 1, 2, 3, 6, 9, 18
+
+common factors = 1, 2, 3, 6
+gcd = 6 
+
+'''
